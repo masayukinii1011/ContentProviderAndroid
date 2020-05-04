0976735636e04adc0747e72dd6666c333c35e968
masayukinii1011 <masayukinii1011@gmail.com>
2020-05-04 12:33:05 +0900

    add comment

--- app/src/main/java/com/example/contentproviderandroid/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderandroid/MainActivity.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //contentResolver.queryで抽出したい項目
     var cols = listOf<String>(
         ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
         ContactsContract.CommonDataKinds.Phone.NUMBER,
@@ -22,6 +23,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /**
+         * パーミッションの許可が無い場合、許可ダイアログを表示
+         */
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.READ_CONTACTS
@@ -37,6 +41,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 許可ダイアログの結果を受け取る
+     */
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -48,30 +55,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 電話帳を読み込んでListViewへ渡す
+     */
     private fun readContact() {
-
-        var from = listOf<String>(
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-            ContactsContract.CommonDataKinds.Phone.NUMBER
-        ).toTypedArray()
-
-        var to = intArrayOf(android.R.id.text1, android.R.id.text2)
-
+        //SQLiteのCursor
+        //(検索したいURI, 抽出したい項目, 絞り込み条件, 絞り込みパラメータ, ソート)
         var rs = contentResolver.query(
             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
             cols, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
         )
 
-        var adapter =
-            SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, rs, from, to, 0)
+        //表示するColumn
+        val from = listOf<String>(
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER
+        ).toTypedArray()
 
+        //バインドするID
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+
+        //ListViewへ渡す
+        //(コンテキスト,表示するレイアウト,SQLiteのCursor,表示するColumn,バインドするID,？)
+        val adapter =
+            SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, rs, from, to, 0)
         listview1.adapter = adapter
 
+        /**
+         * 入力イベントの処理
+         */
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /**
+             * 検索ボタンが押下された時
+             */
             override fun onQueryTextSubmit(p0: String?): Boolean {
                 return false
             }
 
+            /**
+             * テキストが変更された時
+             */
             override fun onQueryTextChange(p0: String?): Boolean {
                 rs = contentResolver.query(
                     ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
@@ -80,6 +103,7 @@ class MainActivity : AppCompatActivity() {
                     Array(1) { "%$p0%" },
                     ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
                 )
+                //変更をアダプタに通知
                 adapter.changeCursor(rs)
                 return false
             }
